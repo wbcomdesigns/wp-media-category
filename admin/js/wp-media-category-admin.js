51988c75e0c732b7bd22acfb26dd47fec5979076
Mohammad Imran <pallavi@wbcomdesigns.com>
2021-08-09 19:22:15 +0530

    Fix media category issue

--- admin/js/wp-media-category-admin.js
+++ admin/js/wp-media-category-admin.js
@@ -40,11 +40,11 @@
       }).render());
     }
   });
-})()
+});
 
 jQuery(document).ready(function() {
   'use strict';
-  jQuery("#bulk-action-selector-top").live('change', function() {
+  jQuery("#bulk-action-selector-top").on('change', function() {
     var selectVal = jQuery('#bulk-action-selector-top :selected').val();
     if (selectVal == 'change_term') {
       var loaderContainer = jQuery('<span/>', {
@@ -72,7 +72,7 @@ jQuery(document).ready(function() {
     }
   });
 
-  jQuery("#bulk-action-selector-bottom").live('change', function() {
+  jQuery("#bulk-action-selector-bottom").on('change', function() {
     var selectVal = jQuery('#bulk-action-selector-bottom :selected').val();
     if (selectVal == 'change_term') {
       var loaderContainer = jQuery('<span/>', {
